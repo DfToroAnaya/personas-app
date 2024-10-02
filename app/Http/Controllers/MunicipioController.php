71c23af9f04890c9c3f8a3dbf3a8464696b592e6
DfToroAnaya <dtoroanaya16@gmail.com>
2024-10-01 23:18:32 -0500

    Municipality Delete

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -79,6 +79,16 @@ class MunicipioController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $municipio =Municipio::find($id);
+        $municipio->delete();
+
+        $municipio = DB::table('tb_municipio')
+        ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+        ->select('tb_municipio.*', 'tb_departamento.depa_nomb')
+        ->get();
+
+        return view('municipio.index', ['municipios' => $municipio]);
+
+
     }
 }
